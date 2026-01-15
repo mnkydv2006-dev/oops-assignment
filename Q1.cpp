7e6d2471f7560ce488ac58fd0e3eb75b27173efc
mnkydv2006-dev <mnkydv2006@gmail.com>
2026-01-15 14:47:53 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,11 @@
+//NAME : MAYANK KUMAR YADAV 
+//COURSE: BTECH(CSE)
+//SEC: "A"
+//ROLL NO: 34
+ #include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! mayank ";
+  return 0;
+}
