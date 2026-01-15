4c41f6753b88408f8bc68d1a4e66d5dc74439a07
MAYANK KUMAR YADAV <mnkydv2006@gmail.com>
2026-01-15 16:25:57 +0530

    Q101-Q110

---
+++ Q101.cpp
@@ -0,0 +1,33 @@
+//NAME :-MAYANK KUMAR YADAV
+//COURSE: BTECH(CSE)
+//SEC:(A) 
+//ROLL NO:-34    
+    
+#include<iostream>
+using namespace std ;
+    
+ class parent{
+    public:
+     virtual void show(){
+        cout<<"this is parent class:-"<<endl;
+
+    }
+ };
+ class child : public parent{
+    public:
+    void show(){
+        cout<<"this is chlid class:-"<<endl;
+    }
+ };        
+    
+int main(){
+    
+    parent *p;
+    child c;
+    p=&c;
+    p->show();
+     
+     
+    
+     return 0;
+}
